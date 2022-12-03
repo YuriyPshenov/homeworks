6471b18e628414f66d80aa68f0fa9c72d36ae127
YuriyPshenov <yuriypshenov@gmail.com>
2022-12-03 19:41:32 +0300

    updating HW7 update

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -47,7 +47,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
                     id={id + '-input-' + o.id}
                     className={finalRadioClassName}
                     type={'radio'}
-                    name={o.name}
+                    name={name}
                     value={o.id}
                     // name, checked, value делают студенты
                     checked={o.id === value}
